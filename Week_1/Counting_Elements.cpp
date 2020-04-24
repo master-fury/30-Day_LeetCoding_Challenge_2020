8a251b46024f688ea492a7c2b73fc46d00520844
Manish Kumar <master20121997@gmail.com>
2020-04-24 23:59:00 +0530

    Counting Elements

---
+++ Week_1/Counting_Elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+      set <int> s;
+        for(int x:arr){
+            s.insert(x);
+        }
+        int count=0;
+        for(int x:arr){
+            if(s.count(x+1)==1)
+                count++;
+        }
+        return count;
+        }
+    
+};
