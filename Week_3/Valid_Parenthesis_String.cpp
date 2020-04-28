9b477f1cb2a7ac63f4be8c6cdacdd2a5a5517df1
Manish Kumar <master20121997@gmail.com>
2020-04-28 23:41:46 +0530

    Valid_Parenthesis_String.cpp

---
+++ Week_3/Valid_Parenthesis_String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int balance=0;
+        const int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='*')
+                balance++;
+            else if(--balance==-1)
+                return false;
+        }
+            balance=0;
+            for(int i=n-1;i>=0;i--){
+               if(s[i]==')' || s[i]=='*')
+                balance++;
+            else if(--balance==-1)
+                return false;
+                
+            }
+     return true; 
+    }
+};
